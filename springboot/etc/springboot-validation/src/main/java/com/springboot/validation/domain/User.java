3c4f9acdfaab3c21839d9cedf725cbefb1db21ad
bgpark <bgpark82@gmail.com>
2020-10-24 17:20:11 +0900

    feat: validation 라이브러리 추가

--- springboot/etc/springboot-validation/src/main/java/com/springboot/validation/domain/User.java
+++ springboot/etc/springboot-validation/src/main/java/com/springboot/validation/domain/User.java
@@ -1,13 +1,17 @@
 package com.springboot.validation.domain;
 
+
 import lombok.Builder;
 import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
 
 @Builder
 @Getter
 public class User {
 
     private Long id;
+    @NotNull
     private String username;
     private String password;
 }
--- springboot/etc/springboot-validation/src/main/java/com/springboot/validation/domain/UserController.java
+++ springboot/etc/springboot-validation/src/main/java/com/springboot/validation/domain/UserController.java
@@ -1,17 +1,18 @@
 package com.springboot.validation.domain;
 
-import com.springboot.validation.exception.UsernameNotValidException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
 
     @PostMapping
-    public User save(@RequestBody User user) {
-        if(user.getUsername() == null){
-            throw new UsernameNotValidException();
-        }
+    public User save(@Valid @RequestBody User user) {
         return user;
     }
 }
