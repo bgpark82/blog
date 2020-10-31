1ab29fe85f062938f9710cc18d8ac051ed32b7ba
bgpark <bgpark82@gmail.com>
2020-10-31 15:32:16 +0900

    feat: 상품 수정 UI

--- springboot/jpa/springboot-shop/src/main/java/com/springboot/jpa/controller/ItemController.java
+++ springboot/jpa/springboot-shop/src/main/java/com/springboot/jpa/controller/ItemController.java
@@ -61,8 +61,13 @@ public class ItemController {
      */
     @PostMapping(value = "/items/{itemId}/edit")
     public String updateItem(@ModelAttribute("form") BookForm form) {
-        Book book = new Book(); book.setId(form.getId()); book.setName(form.getName());
-        book.setPrice(form.getPrice()); book.setStockQuantity(form.getStockQuantity()); book.setAuthor(form.getAuthor()); book.setIsbn(form.getIsbn());
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
         itemService.saveItem(book);
         return "redirect:/items";
     }
