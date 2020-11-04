a0d506b4e54b461611e5d56000f1450f0173bb01
bgpark <bgpark82@gmail.com>
2020-11-05 01:17:48 +0900

    스프링부트2 활용: 주문조회 엔티티를 DTO로 전환 - 페이징과 한계돌파 V4

--- springboot/jpa/springboot-shop/src/main/java/com/springboot/jpa/api/OrderApiController.java
+++ springboot/jpa/springboot-shop/src/main/java/com/springboot/jpa/api/OrderApiController.java
@@ -9,6 +9,7 @@ import com.springboot.jpa.repository.OrderRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -74,6 +75,29 @@ public class OrderApiController {
             System.out.println("ordre ref=" + order + " id=" + order.getId());
         }
 
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(toList());
+        return result;
+    }
+
+    /**
+     * V3.1 엔티티를 조회해서 DTO로 변환 페이징 고려
+     *-ToOne 관계만 우선 모두 페치 조인으로 최적화
+     * - 컬렉션 관계는 hibernate.default_batch_fetch_size, @BatchSize로 최적화 */
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3Paging(
+            @RequestParam(value = "offset",defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue= "100") int limit) {
+        // 1. ToOne을 Fetch로 먼저 호
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+
+        // 같은 객체가 반복된다
+        for (Order order: orders) {
+            System.out.println("ordre ref=" + order + " id=" + order.getId());
+        }
+
+        // 2. 지연로딩 발생 -> 프록시 초기화
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
                 .collect(toList());
--- springboot/jpa/springboot-shop/src/main/java/com/springboot/jpa/repository/OrderRepository.java
+++ springboot/jpa/springboot-shop/src/main/java/com/springboot/jpa/repository/OrderRepository.java
@@ -85,6 +85,16 @@ public class OrderRepository {
                 " join fetch o.delivery d", Order.class) .getResultList();
     }
 
+    // 객체를 한방 쿼리로 가져오고 싶을 때
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery("select o from Order o" +
+                " join fetch o.member m" +
+                " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
     public List<Order> findAllWithItem() {
         /**
          * distinct
