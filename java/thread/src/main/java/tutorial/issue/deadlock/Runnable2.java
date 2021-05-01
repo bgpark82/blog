31eb89c5c7736b9c4fe05967e80ee8a1fb3048b2
bgpark <bgpark82@gmail.com>
2021-05-01 09:41:50 +0900

    feat: Deadlock 예제 추가

---
+++ java/thread/src/main/java/tutorial/issue/deadlock/Runnable2.java
@@ -0,0 +1,39 @@
+package tutorial.issue.deadlock;
+
+import java.util.concurrent.locks.Lock;
+
+public class Runnable2 implements Runnable{
+
+    private Lock lock1 = null;
+    private Lock lock2 = null;
+
+    public Runnable2(Lock lock1, Lock lock2) {
+        this.lock1 = lock1;
+        this.lock2 = lock2;
+    }
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        System.out.println(name + " attempting to lock Lock2");
+        lock2.lock();
+        System.out.println(name + " locked Lock2");
+
+
+        // lock 실패시 넘어가지 않음
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(name + " attempting to lock Lock1");
+        lock1.lock();
+        System.out.println(name + " locked Lock1");
+
+        System.out.println(name + " unlock Lock1");
+        lock1.unlock();
+        System.out.println(name + " unlock Lock2");
+        lock2.unlock();
+    }
+}
