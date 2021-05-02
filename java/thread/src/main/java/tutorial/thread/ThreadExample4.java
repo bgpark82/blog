62f8088b94a8486a0b94eab189ea2955a833eabb
bgpark <bgpark82@gmail.com>
2021-05-03 00:08:20 +0900

    minor change

--- java/thread/src/main/java/tutorial/thread/ThreadExample4.java
+++ java/thread/src/main/java/tutorial/thread/ThreadExample4.java
@@ -3,49 +3,49 @@ package tutorial.thread;
 public class ThreadExample4 {
 
     public static void main(String[] args) {
-        StoppabbleRunnable stoppabbleRunnable = new StoppabbleRunnable();
-        Thread thread = new Thread(stoppabbleRunnable, "Stoppable Thread");
+        StopRunnable stopRunnable = new StopRunnable();
+        Thread thread = new Thread(stopRunnable);
         thread.start();
 
         try {
-            System.out.println(Thread.currentThread().getName() + "is in sleep for 5 sec..");
+            System.out.println(Thread.currentThread().getName() + " is in sleep for 5 sec..");
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        stoppabbleRunnable.requestStop();
+        stopRunnable.stop();
         System.out.println("Stop Requested!");
     }
 
-    public static class StoppabbleRunnable implements Runnable {
+    public static class StopRunnable implements Runnable {
 
-        private boolean stopRequested = false;
+        private boolean stop = false;
 
-        public synchronized void requestStop() {
-            this.stopRequested = true;
+        @Override
+        public void run() {
+            System.out.println("StopRunnable started");
+            while(!isStop()) {
+                sleep(1000);
+            }
+            System.out.println("StopRunnable stopped");
         }
 
-        public synchronized boolean isStopRequested() {
-            return this.stopRequested;
-        }
-
-        private void sleep(long millis) {
+        private void sleep(int i) {
             try {
-                System.out.println(Thread.currentThread().getName() + "is in sleep...");
-                Thread.sleep(millis);
+                System.out.println("StopRunnable in sleep...");
+                Thread.sleep(i);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
 
-        @Override
-        public void run() {
-            System.out.println("StoppabbleRunabble Running");
-            while (!isStopRequested()) {
-                sleep(1000);
-            }
-            System.out.println("StoppabbleRunabble Stopped");
+        private synchronized boolean isStop() {
+            return this.stop;
+        }
+
+        public synchronized void stop() {
+            this.stop = true;
         }
     }
 }
