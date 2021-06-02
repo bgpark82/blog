f77388b9d149c70f7578803f6b6e575d6cb12583
bgpark <bgpark@citus.co.kr>
2021-06-02 16:53:00 +0900

    redis

--- springboot/redis/springboot-redis/src/main/java/com/bgpark/redis/domain/person/Person.java
+++ springboot/redis/springboot-redis/src/main/java/com/bgpark/redis/domain/person/Person.java
@@ -1,12 +1,12 @@
 package com.bgpark.redis.domain.person;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+@ToString
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(of = "name")
 public class Person {
 
     private String name;
