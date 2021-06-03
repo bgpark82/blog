fadd11ee035dc596adb28db59ec411b48ef3368c
bgpark <bgpark@citus.co.kr>
2021-06-03 13:36:03 +0900

    feat: put, get 메소드 작성

--- java/library/src/main/java/com/bgpark/library/CustomHashMap.java
+++ java/library/src/main/java/com/bgpark/library/CustomHashMap.java
@@ -1,9 +1,14 @@
 package com.bgpark.library;
 
-public class CustomHashMap {
+import java.util.Objects;
+
+public class CustomHashMap<K,V> {
 
     static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
     static final int MAXIMUM_CAPACITY = 1 << 30;
+
+    transient Node<K,V>[] table;
 
     final float loadFactor;
     int threshold;
@@ -23,7 +28,11 @@ public class CustomHashMap {
             throw new IllegalArgumentException("음수 불가");
         }
         this.loadFactor = loadFactor;
-        this.threshold = initialCapacity;
+        this.threshold = tableSizeFor(initialCapacity);
+    }
+
+    public CustomHashMap(int initialCapacity) {
+        this(initialCapacity, DEFAULT_LOAD_FACTOR);
     }
 
     // 만약 capacity가 10이라면 10은 2^3과 2^4 사이의 값이므로 2^4 = 16을 반환한다
@@ -34,6 +43,114 @@ public class CustomHashMap {
         n |= n >>> 4;
         n |= n >>> 8;
         n |= n >>> 16;
-        return n + 1;
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
+
+    static final int hash(Object key) {
+        int h;
+        return (key == null) ? 1 : (h = key.hashCode()) ^ (h >>> 16);
+    }
+
+    static Node newNode(int hash, String key, String value, Node next) {
+        return new Node(hash, key, value, next);
+    }
+
+    final String putVal(int hash, String key, String value) {
+        Node[] curTable;
+        Node p;
+        int length;
+        int i;
+
+        // 테이블이 null이거나 비어있는 경우
+        if((curTable = table) == null || (length = curTable.length) == 0) {
+            length = (curTable = resize()).length;
+        }
+
+        // 해당 키에 값이 없으면 새로운 노드 추가
+        if((p = curTable[i = getKey(hash, length)]) == null) {
+            curTable[i] = newNode(hash, key, value, null);
+        }
+
+        return null;
+    }
+
+    final Node[] resize() {
+        Node[] oldTable = table;
+
+        int oldCapacity = (oldTable == null) ? 0 : oldTable.length;
+        int oldThreshold = threshold;
+        int newCapacity = 0;
+        int newThreshold = 0;
+
+
+        newCapacity = DEFAULT_INITIAL_CAPACITY;
+        newThreshold = (int)(DEFAULT_LOAD_FACTOR * DEFAULT_INITIAL_CAPACITY);
+
+        Node[] newTable = new Node[newCapacity];
+        threshold = newThreshold;
+        table = newTable;
+
+        return newTable;
+    }
+
+    public void put(String key, String value) {
+        putVal(hash(key), key, value);
+    }
+
+    public V get(Object key) {
+        Node<K,V> node;
+        return (node = getNode(hash(key), key)) == null ? null : node.value;
+    }
+
+    final Node<K,V> getNode(int hash, Object key) {
+        Node<K,V>[] curTable;
+        Node<K,V> first;
+        int length;
+        K k;
+
+        if((curTable = table) != null &&
+            (length = table.length) > 0 &&
+            (first = curTable[getKey(hash, length)]) != null) {
+
+            if(first.hash == hash &&
+                    ((k = first.key) == key || (key != null && key.equals(k)))) {
+                return first;
+            }
+        }
+
+        return null;
+    }
+
+    final int getKey(int hash, int length) {
+        return (length - 1) & hash;
+    }
+
+
+    static class Node<K,V> {
+
+        final int hash;
+        final K key;
+        V value;
+        Node<K,V> next;
+
+        public Node(int hash, K key, V value, Node next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node node = (Node) o;
+            return hash == node.hash && Objects.equals(key, node.key) && Objects.equals(value, node.value) && Objects.equals(next, node.next);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(hash, key, value, next);
+        }
     }
 }
--- java/library/src/test/java/com/bgpark/library/CustomHashMapTest.java
+++ java/library/src/test/java/com/bgpark/library/CustomHashMapTest.java
@@ -2,6 +2,7 @@ package com.bgpark.library;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -92,10 +93,148 @@ class CustomHashMapTest {
     }
 
     @Test
-    void tableSizeFor() {
+    void constructor10() {
+        CustomHashMap map = new CustomHashMap(10);
+        assertThat(map.loadFactor).isEqualTo(0.75f);
+        assertThat(map.threshold).isEqualTo(1 << 4);
+    }
+
+    @Test
+    void tableSizeFor1() {
         int result1 = CustomHashMap.tableSizeFor(3); // 2^1 ~ 2^2
         assertThat(result1).isEqualTo((int)Math.pow(2, 2));
+
         int result2 = CustomHashMap.tableSizeFor(10); // 2^3 ~ 2^4
         assertThat(result2).isEqualTo((int)Math.pow(2, 4));
+
+        int result3 = CustomHashMap.tableSizeFor(1<<5); // 2^3 ~ 2^4
+        assertThat(result3).isEqualTo((int)Math.pow(2, 5));
+    }
+
+    @Test
+    void tableSizeFor2() {
+        int result1 = CustomHashMap.tableSizeFor(-10); // 2^1 ~ 2^2
+        assertThat(result1).isEqualTo(1);
+    }
+
+//        https://mincong.io/2018/04/08/learning-hashmap/
+//        https://www.zhihu.com/question/20733617
+//        https://d2.naver.com/helloworld/831311
+//        https://coderoad.ru/45125497/%D0%97%D0%B0%D1%87%D0%B5%D0%BC-%D0%B2%D0%BE%D0%B7%D0%B2%D1%80%D0%B0%D1%89%D0%B0%D1%82%D1%8C-h-key-hashCode-h-16-%D0%BA%D1%80%D0%BE%D0%BC%D0%B5-key-hashcode
+//        hash ^ (hash >> 16)
+//        hash                = 11111111 11111111 11110000 11101010
+//        hash >> 16          = 00000000 00000000 11111111 11111111
+//        hash ^ hash >> 16   = 11111111 11111111 00001111 00010101
+//        ^ : 50/50
+//        | : 75/25
+//        & : 25/75
+    @Test
+    void put() {
+        int h = "key-value".hashCode();
+        System.out.println(Integer.toBinaryString((h)));
+        System.out.println(Integer.toBinaryString(h >>> 16));
+        System.out.println(Integer.toBinaryString(h ^ (h >>> 16)));
+        System.out.println(h >>> 16);
+        System.out.println(h ^ (h >>> 16));
+    }
+
+    @Test
+    void put2() {
+        CustomHashMap map = new CustomHashMap();
+        map.put("key", "value");
+        assertThat(map.table.length).isEqualTo(16);
+        assertThat(map.table).contains(CustomHashMap.newNode(CustomHashMap.hash("key"), "key", "value", null));
+        assertThat(map.threshold).isEqualTo(12);
+    }
+
+    @Test
+    void hash() {
+        int hash = CustomHashMap.hash("key");
+
+        assertThat(hash).isEqualTo(106078);
+        assertThat(Integer.toBinaryString("key".hashCode())).isEqualTo("11001111001011111");
+        assertThat(Integer.toBinaryString("key".hashCode() >> 16)).isEqualTo("1");
+        assertThat(Integer.toBinaryString(hash)).isEqualTo("11001111001011110");
+    }
+
+    @Test
+    void hash2() {
+        int hash = CustomHashMap.hash(null);
+
+        assertThat(hash).isEqualTo(1);
+    }
+
+    @Test
+    void putVal() {
+        int hash = CustomHashMap.hash("key");
+        CustomHashMap map = new CustomHashMap();
+
+        map.putVal(hash, "key", "value");
+
+        assertThat(map.table.length).isEqualTo(16);
+        assertThat(map.table).contains(CustomHashMap.newNode(hash, "key", "value", null));
+    }
+
+    @Test
+    void newNode() {
+        int hash = CustomHashMap.hash("key");
+
+        CustomHashMap.Node node = CustomHashMap.newNode(hash, "key", "value", null);
+
+        assertThat(node.hash).isEqualTo(hash);
+        assertThat(node.key).isEqualTo("key");
+        assertThat(node.value).isEqualTo("value");
+        assertThat(node.next).isEqualTo(null);
+    }
+
+    @Test
+    void resize1() {
+        float DEFAULT_LOAD_FACTOR = 0.75f;
+        int DEFAULT_INITIAL_CAPACITY = 1 << 4;
+
+        assertThat(DEFAULT_LOAD_FACTOR * DEFAULT_INITIAL_CAPACITY).isEqualTo(12);
+    }
+
+    @Test
+    void resize2() {
+        CustomHashMap map = new CustomHashMap();
+
+        CustomHashMap.Node[] table = map.resize();
+
+        assertThat(table.length).isEqualTo(16);
+        assertThat(map.threshold).isEqualTo(12);
+    }
+
+    @Test
+    void get() {
+        CustomHashMap<String, String> map = new CustomHashMap();
+
+        String value = map.get("key");
+
+        assertThat(value).isNull();
+    }
+
+    @Test
+    void get2() {
+        CustomHashMap<String, String> map = new CustomHashMap();
+        map.put("key","value");
+
+        String value = map.get("key");
+
+        assertThat(value).isEqualTo("value");
+    }
+
+    @Test
+    void getNode() {
+        int hash = CustomHashMap.hash("key");
+        CustomHashMap<String, String> map = new CustomHashMap();
+        map.put("key","value");
+
+        CustomHashMap.Node<String, String> node = map.getNode(hash, "key");
+
+        assertThat(node.key).isEqualTo("key");
+        assertThat(node.value).isEqualTo("value");
+        assertThat(node.hash).isEqualTo(hash);
+        assertThat(node.next).isNull();
     }
 }
